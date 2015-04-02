d00ee2e4263813b0b5852fdb6c37922ba8be540a
Example <dayrobis@jovenclub.cu>
2015-04-02 10:33:38 -0400

    Opciones programadas  - mostrar y oculptar puntos  - color de los puntos del borde y de relleno  - mostrar y ocultar area  - color del area y opacidad  - mostrar y ocultar grillas, tanto eje x como eje y

--- js/linea.js
+++ js/linea.js
@@ -8,13 +8,18 @@
         self.raiz = raiz;
         self.opciones = opciones;
         self.default_options = {
+
+            showArea: true, //muestra el area sombreada bajo la linea
+            area: {color: 'lightsteelblue', opacity: '0.5'},
+
             showgridLines: true,
-            gridLines: {color: 'lightgrey', showHorizontal: true, showVertical: true},
+            gridLines: {color: 'lightgrey', horizontal: true, vertical: true},
+            ticks: {x: 10, y: 6, gridX: 10, gridY: 10}, //ticks del eje x y y y las grillas
 
             fontFamily: "Arial",// la Fuente
             fontSize: 12,// la Fuente
 
-            dateFormat: "dd-mm-yy", // Formato de la fecha
+            dateFormat: "%Y-%m-%d", // Formato de la fecha
 
             animation: true, //Animada
             animacionType: '', // tipo de animacion
@@ -23,7 +28,7 @@
             tooltip: {fontFamily: 'Arial', fontColor: 'white', fontSize: 12, fillColor: 'black', borderColor: 'white'},
 
             showPoint: true,
-            point: {pointRadius: 3, pointBorderWidth: 1, fillColor: '', borderColor: 'blue'},
+            point: {radio: 2, bordeWidth: 0.5, color: 'black', borderColor: 'steelblue'},
 
             showPeriod: true,
             periodos: [
@@ -80,9 +85,8 @@
                 }
             ],
 
-            mouseLineIndicator: {horizontal: true, vertical: true}, //muestra linea horizontal y vertical cuando se mueve el mouse por la grafica
-            xTicks: 6,
-            yTicks: 5
+            mouseLineIndicator: {horizontal: true, vertical: true} //muestra linea horizontal y vertical cuando se mueve el mouse por la grafica
+
         };
         self.titulo = "LineChart by IRStrat";
         self.datos = [
@@ -104,8 +108,8 @@
 
         //--------------------- Variables privadas aqui--------------------------
 
-        var margin = {top: 5, right: 10, bottom: 25, left: 30};
-        var width = 480 - margin.left - margin.right, height = 250 - margin.top - margin.bottom;
+        var margin = {top: 5, right: 0, bottom: 25, left: 30};
+        var width = 800 - margin.left - margin.right, height = 400 - margin.top - margin.bottom;
 
         var colors = d3.scale.category10();//escala de 10 colores de d3
         var parseDate = d3.time.format("%d-%b-%y").parse; //Formato por defecto de la fecha
@@ -115,6 +119,8 @@
         var periodHeight; //height por defecto del div contenedor de los periodos
         var fechaInicio, fechaFin; //fecha inicial y fecha final selesccionada
         var inicioDominio, finDominio; //Inicio y fin de los datos cargados
+        var area; //rellenar el area debajo de la grafica
+        var animationTime;
 
 
         //Mezcla las opciones con las que pasa el usuario
@@ -152,25 +158,72 @@
                 g_main = svg.select('.g_main');
 
 
-                //  Adiciona el eje X
-                g_main.append("g")
-                    .attr("class", "x axis")
-                    .attr("transform", "translate(0," + height + ")")
-                    .call(xAxis);
+
+                if (self.default_options.showgridLines) {
+                    if (self.default_options.gridLines.vertical) {
+                        //Grillas verticales
+                        g_main.append("g")
+                            .attr("class", "grid x")
+                            .attr("transform", "translate(0," + height + ")")
+                            .call(_grillas_eje_x().tickSize(-height, 0, 0).tickFormat(""));
+                    }
+
+                    if (self.default_options.gridLines.horizontal) {
+                        //Grillas horizontales
+                        g_main.append("g")
+                            .attr("class", "grid y")
+                            .call(_grillas_eje_y().tickSize(-width, 0, 0).tickFormat(""));
+                    }
+                }
+
+                if (self.default_options.showArea) {
+                    //Dibuja el area debajo de la grafica
+                    g_main.append("path")
+                        .datum(data)
+                        .attr("class", "area")
+                        .attr("d", area);
+                }
+
+                //Dibuja la linea
+                g_main.append("path")
+                    .datum(data)
+                    .attr("clip-path", "url(#clip)")
+                    .attr("class", "line line-main")
+                    .attr("stroke", colors(3))
+                    .attr("stroke-width", 2)
+                    .attr("d", valueline);
 
                 //  Adiciona el Eje Y
                 g_main.append("g")
                     .attr("class", "y axis")
                     .call(yAxis);
 
-                //Add the valueline path.
-                g_main.append("path")
-                    .datum(data)
-                    .attr("clip-path", "url(#clip)")
-                    .attr("class", "line line-main")
-                    .attr("stroke", colors(0))
-                    .attr("d", valueline);
+                //  Adiciona el eje X
+                g_main.append("g")
+                    .attr("class", "x axis")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(xAxis);
 
+                if (self.default_options.showPoint) {
+                    g_main.selectAll('circle.mark').data(data).enter().append('circle')
+                        .attr('class', 'mark')
+                        .attr('fill', self.default_options.point.color)
+                        .attr('stroke', self.default_options.point.borderColor)
+                        .attr('stroke-width', self.default_options.point.bordeWidth)
+                        .attr('cx', function (d) {
+                            return x(d.date);
+                        })
+                        .attr('cy', function (d) {
+                            return y(d.close);
+                        })
+                        .attr('r', self.default_options.point.radio)
+                        .on('mouseover', function (d, i) {
+                            d3.select(this).transition().attr('r', (self.default_options.point.radio + 4));
+                        })
+                        .on('mouseout', function (d, i) {
+                            d3.select(this).transition().attr('r', self.default_options.point.radio);
+                        })
+                }
 
             } else {
                 throw new Error("Formato de los datos incorrectos");
@@ -242,8 +295,22 @@
                 y = d3.scale.linear().range([height, 0]);
 
                 //Eje X and Y
-                xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(self.default_options.xTicks);
-                yAxis = d3.svg.axis().scale(y).orient("left").ticks(self.default_options.yTicks);
+                xAxis = d3.svg.axis().scale(x).orient("bottom")
+                    .ticks(self.default_options.ticks.x)
+                    .tickFormat(d3.time.format(self.default_options.dateFormat));
+
+                yAxis = d3.svg.axis().scale(y).orient("left")
+                    .ticks(self.default_options.ticks.y);
+
+                //Area debajo de la grafica
+                area = d3.svg.area()
+                    .x(function (d) {
+                        return x(d.date);
+                    })
+                    .y0(height)
+                    .y1(function (d) {
+                        return y(d.close);
+                    });
 
                 // Define que valores va a graficar la linea para cada eje
                 valueline = d3.svg.line()
@@ -253,6 +320,9 @@
                     .y(function (d) {
                         return y(d.close);
                     });
+
+                animationTime = 750; //750 milisegundos
+
             }
 
             //Crear los periodos
@@ -284,6 +354,14 @@
             }
             return true;
 
+        };
+
+        _grillas_eje_x = function () {
+            return d3.svg.axis().scale(x).orient("bottom").ticks(self.default_options.ticks.gridX);
+        };
+
+        _grillas_eje_y = function () {
+            return d3.svg.axis().scale(y).orient("left").ticks(self.default_options.ticks.gridY);
         };
 
         //Llama a este metodo para inicializar las opciones de configuracion que sepasaron por parametro
